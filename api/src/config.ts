dc1fe59c5e034b4947fa6dd055499efe6b04842e
Danylo Oliveira <danylo.oliveira@useargo.com>
2025-09-05 11:09:28 -0300

    remove kafka da api

--- api/src/config.ts
+++ api/src/config.ts
@@ -5,6 +5,7 @@ export const config = {
   host: process.env.HOST ?? "0.0.0.0",
   kafkaBrokers: (process.env.KAFKA_BROKERS ?? "localhost:19092").split(","),
   redisUrl: process.env.REDIS_URL ?? "redis://localhost:6379",
-  mockKafka: !isProd && process.env.USE_REAL_KAFKA !== "true",
+  // Allow disabling Kafka regardless of NODE_ENV using USE_REAL_KAFKA
+  mockKafka: process.env.USE_REAL_KAFKA !== "true",
   mockRedis: !isProd && process.env.USE_REAL_REDIS !== "true",
 };
